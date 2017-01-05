88fca6722bd03848978dff876eaa318f63c441ec
Stefan Starflinger <stefanstarflinger@eduroam-004-160.wlan.univie.ac.at>
2017-01-05 15:38:12 +0100

    setup

---
+++ config.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.6.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBXl1fqqllplpJQXC-wR-Ay3qTpQUV4ZKY",
+    authDomain: "abgabe4.firebaseapp.com",
+    databaseURL: "https://abgabe4.firebaseio.com",
+    storageBucket: "abgabe4.appspot.com",
+    messagingSenderId: "692350034443"
+  };
+  firebase.initializeApp(config);
+</script>
